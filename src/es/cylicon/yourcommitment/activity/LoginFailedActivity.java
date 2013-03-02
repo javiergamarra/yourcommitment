a3f8946d1cd8044414f029600ea7e1b74c8ec487
Javier Gamarra <nhpatt@gmail.com>
2013-03-02 12:31:56 +0100

    resources

--- src/es/cylicon/yourcommitment/activity/LoginFailedActivity.java
+++ src/es/cylicon/yourcommitment/activity/LoginFailedActivity.java
@@ -1,25 +1,15 @@
 package es.cylicon.yourcommitment.activity;
 
-import es.cylicon.yourcommitment.R;
-import es.cylicon.yourcommitment.R.layout;
-import es.cylicon.yourcommitment.R.menu;
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import es.cylicon.yourcommitment.R;
 
 public class LoginFailedActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login_failed);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login_failed, menu);
-		return true;
 	}
 
 }
--- src/es/cylicon/yourcommitment/activity/MenuActivity.java
+++ src/es/cylicon/yourcommitment/activity/MenuActivity.java
@@ -1,41 +1,9 @@
 package es.cylicon.yourcommitment.activity;
 
-import java.util.List;
-
 import roboguice.activity.RoboFragmentActivity;
-import android.util.Log;
-
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.SaveCallback;
 
 public class MenuActivity extends RoboFragmentActivity {
 
 	protected static final String TAG = "YOUR_COMMITMENT";
-
-	private void searchSomething() {
-		final ParseQuery query = new ParseQuery("GameScore");
-		try {
-			final List<ParseObject> objects = query.find();
-			Log.e(TAG, objects.toString());
-		} catch (final ParseException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void saveSomething() {
-		final ParseObject gameScore = new ParseObject("GameScore");
-		gameScore.put("score", 1337);
-		gameScore.put("playerName", "Sean Plott");
-		gameScore.put("cheatMode", false);
-		gameScore.saveInBackground(new SaveCallback() {
-
-			@Override
-			public void done(final ParseException arg0) {
-				Log.e(TAG, "done");
-			}
-		});
-	}
 
 }
