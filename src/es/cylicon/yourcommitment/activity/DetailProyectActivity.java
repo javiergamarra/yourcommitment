b2aad88c17cb9a10f212b0d89688c1ec9f5a1334
Javier Gamarra <nhpatt@gmail.com>
2013-03-02 18:09:40 +0100

    things

--- src/es/cylicon/yourcommitment/activity/DetailProyectActivity.java
+++ src/es/cylicon/yourcommitment/activity/DetailProyectActivity.java
@@ -2,68 +2,61 @@ package es.cylicon.yourcommitment.activity;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
-import android.app.ListActivity;
+
 import es.cylicon.yourcommitment.R;
-import es.cylicon.yourcommitment.R.layout;
-import es.cylicon.yourcommitment.R.menu;
-import es.cylicon.yourcommitment.adapter.ProyectAdapter;
 import es.cylicon.yourcommitment.adapter.UpdateAdapter;
 import es.cylicon.yourcommitment.model.Proyect;
-import android.os.Bundle;
-import android.app.Activity;
-import android.util.Log;
-import android.view.Menu;
 import es.cylicon.yourcommitment.model.Update;
 
+public class DetailProyectActivity extends MenuActivity {
 
-
-public class DetailProyectActivity extends ListActivity {
-
-	private static final String TAG = "ProyectsActivity";
-	private List<Update> updates = new ArrayList<Update>();
+	private final List<Update> updates = new ArrayList<Update>();
 	private UpdateAdapter adapter;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail_proyect);
 
-		Bundle extras = getIntent().getExtras();
-		int proyectId = extras.getInt("proyectId");
-		
-		
-		loadUpdates();
+		final Proyect proyect = (Proyect) getIntent().getExtras()
+				.getSerializable("proyect");
+		loadUpdates(proyect.getId());
+
+		final ListView listView = (ListView) findViewById(android.R.id.list);
 		adapter = new UpdateAdapter(this, android.R.layout.simple_list_item_1,
 				updates);
-		setListAdapter(adapter);
+		listView.setAdapter(adapter);
 
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.detail_detail_proyect, menu);
-		return true;
-	}
-
-	private void loadUpdates() {
-		ParseQuery query = new ParseQuery("Update");
-		query.findInBackground(new FindCallback() {
-			public void done(List<ParseObject> parseUpdates, ParseException e) {
-				if (e == null) {
-					for (ParseObject parseUpdate : parseUpdates) {
-						updates.add(new Update(parseUpdate));
-						adapter.notifyDataSetChanged();
+	private void loadUpdates(final String proyectId) {
+		final ParseQuery query = new ParseQuery("Update");
+		query.whereEqualTo("proyectId", proyectId).findInBackground(
+				new FindCallback() {
+					@Override
+					public void done(final List<ParseObject> parseUpdates,
+							final ParseException e) {
+						if (e == null) {
+							for (final ParseObject parseUpdate : parseUpdates) {
+								updates.add(new Update(parseUpdate));
+								adapter.notifyDataSetChanged();
+							}
+						} else {
+							Toast.makeText(DetailProyectActivity.this,
+									"Error al buscar proyectos: ",
+									Toast.LENGTH_SHORT).show();
+						}
 					}
-				} else {
-					Log.e(TAG, "Error al buscar proyectos: " + e.getMessage());
-				}
-			}
-		});
+				});
 	}
 
 }
--- src/es/cylicon/yourcommitment/model/Update.java
+++ src/es/cylicon/yourcommitment/model/Update.java
@@ -8,16 +8,18 @@ public class Update implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	private String id;
 	private String description;
 	private String image;
+	private String proyectId;
 
-	
 	public Update(final ParseObject parseProyect) {
+		id = parseProyect.getObjectId();
 		setDescription(parseProyect.getString("description"));
 		setImage(parseProyect.getString("image"));
+		setProyectId(parseProyect.getString("proyectId"));
 	}
-	
-	
+
 	public String getDescription() {
 		return description;
 	}
@@ -32,6 +34,22 @@ public class Update implements Serializable {
 
 	public void setImage(final String image) {
 		this.image = image;
+	}
+
+	public String getProyectId() {
+		return proyectId;
+	}
+
+	public void setProyectId(final String proyectId) {
+		this.proyectId = proyectId;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(final String id) {
+		this.id = id;
 	}
 
 }
