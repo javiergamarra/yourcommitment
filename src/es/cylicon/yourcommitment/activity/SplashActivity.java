b60c835f08c35241c192aa0a6114bbe72c82f126
andressedano <andress@tid.es>
2013-03-02 13:30:13 +0100

    andres5

--- src/es/cylicon/yourcommitment/activity/SplashActivity.java
+++ src/es/cylicon/yourcommitment/activity/SplashActivity.java
@@ -22,32 +22,16 @@ public class SplashActivity extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
 
-		startAnimating();
+		startAnimate();
 	}
 
-	private void startAnimating() {
-
-		// Movimiento barra colores
-		ImageView barraColores = (ImageView) findViewById(R.id.ImagenViewTop);
-		Animation movimientoVertical = AnimationUtils.loadAnimation(this,
-				R.anim.translate_anim);
-		barraColores.startAnimation(movimientoVertical);
-
+	private void startAnimate() {
 		// Zoom logotipo
 		ImageView logotipoAndroid = (ImageView) findViewById(R.id.ImagenViewLogo);
 		Animation zoomAnimation = AnimationUtils.loadAnimation(this,
 				R.anim.zoom_anim);
 		logotipoAndroid.startAnimation(zoomAnimation);
 
-		// Fade Titulo
-		TextView tituloPrincipal = (TextView) findViewById(R.id.TextViewTitulo);
-		Animation fade1 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-		tituloPrincipal.startAnimation(fade1);
-
-		// Fade subTitulo
-		TextView subtitulo = (TextView) findViewById(R.id.TextViewSubtitulo);
-		Animation fade2 = AnimationUtils.loadAnimation(this, R.anim.fade_in2);
-		subtitulo.startAnimation(fade2);
 
 		// Animacion tiempo final
 		TextView pie = (TextView) findViewById(R.id.TextViewPie);
@@ -72,19 +56,6 @@ public class SplashActivity extends Activity {
 			}
 		});
 
-		// animaciones para TableView
-		Animation spinin = AnimationUtils.loadAnimation(this,
-				R.anim.custom_anim);
-		LayoutAnimationController control = new LayoutAnimationController(
-				spinin);
-
-		TableLayout tabla = (TableLayout) findViewById(R.id.TableLayout01);
-
-		for (int i = 0; i < tabla.getChildCount(); i++) {
-
-			TableRow row = (TableRow) tabla.getChildAt(i);
-			row.setLayoutAnimation(control);
-		}
 
 	}
 
