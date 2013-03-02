f48c901f032aa120e77020dc64b63ad7e9ce4d4e
Soraya Vay <vay1982@gmail.com>
2013-03-02 10:33:43 +0100

    Actividad Usuario

--- gen/es/cylicon/yourcommitment/R.java
+++ gen/es/cylicon/yourcommitment/R.java
@@ -24,22 +24,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
+        public static final int action_settings=0x7f080002;
         public static final int menu=0x7f080000;
+        public static final int menu_settings=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int menu=0x7f030001;
+        public static final int activity_user=0x7f030001;
+        public static final int menu=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int activity_user=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int app_id=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050005;
         public static final int hi=0x7f050003;
         public static final int imagen=0x7f050004;
+        public static final int menu_settings=0x7f050006;
         public static final int opcion1=0x7f050002;
+        public static final int title_activity_user=0x7f050007;
     }
     public static final class style {
         /** 
