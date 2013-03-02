61b2b8b3d5cb76e74c32ba354cf8c10291294891
andressedano <andress@tid.es>
2013-03-02 12:22:15 +0100

    Andres3

--- src/es/cylicon/yourcommitment/activity/MenuActivity.java
+++ src/es/cylicon/yourcommitment/activity/MenuActivity.java
@@ -1,7 +1,25 @@
 package es.cylicon.yourcommitment.activity;
 
-import android.support.v4.app.FragmentActivity;
+import es.cylicon.yourcommitment.R;
+import es.cylicon.yourcommitment.R.layout;
+import es.cylicon.yourcommitment.R.menu;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
 
-public class MenuActivity extends FragmentActivity {
+public class MenuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_menu);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return true;
+	}
 
 }
