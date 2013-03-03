87533f8187d0a2d9d22f4b69f60b869f63ab80b9
Javier Gamarra <nhpatt@gmail.com>
2013-03-03 09:47:04 +0100

    mapa

--- src/es/cylicon/yourcommitment/MapActivity.java
+++ src/es/cylicon/yourcommitment/MapActivity.java
@@ -3,6 +3,7 @@ package es.cylicon.yourcommitment;
 import roboguice.inject.InjectView;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import es.cylicon.yourcommitment.activity.MenuActivity;
 
 public class MapActivity extends MenuActivity {
@@ -16,6 +17,14 @@ public class MapActivity extends MenuActivity {
 		setContentView(R.layout.activity_mapa);
 		webView.getSettings().setJavaScriptEnabled(true);
 		webView.getSettings().setPluginsEnabled(true);
+		webView.setWebViewClient(new WebViewClient() {
+			@Override
+			public void onPageFinished(final WebView view, final String url) {
+				webView.loadUrl("javascript:(function() { "
+						+ "document.getElementsByClassName('leyend-wrapper')[0].style.display = 'none'; "
+						+ "})()");
+			}
+		});
 		webView.loadUrl("http://yourcommitment.org/");
 	}
 }
